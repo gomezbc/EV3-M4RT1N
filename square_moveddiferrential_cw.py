32fa3ffb459d8dcf8489cda65c079be3dbd146e4
ALEX RIVAS MACHIN <123485397+AlexRivasMachin@users.noreply.github.com>
2024-09-30 16:59:03 +0200

    Update square_moveddiferrential_cw.py

--- square_moveddiferrential_cw.py
+++ square_moveddiferrential_cw.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
 from ev3dev2.motor import MoveDifferential, SpeedRPM, OUTPUT_C, OUTPUT_A
-from ev3dev2.wheel import EV3EducationSetTire # gurpilaren zirkunferentzia: 176mm
+from ev3dev2.wheel import EV3EducationSetTire # wheel gurpilaren zirkunferentzia: 176mm
 
 from time import sleep
 
+wheel_circumference = 176  # mm
+side_length = 800  # mm (80 cm)
+
 def main():
-    mdiff = MoveDifferential(OUTPUT_A, OUTPUT_C, EV3EducationSetTire, 16)
+    mdiff = MoveDifferential(OUTPUT_A, OUTPUT_C, EV3EducationSetTire, 118)
     mdiff.odometry_start(theta_degrees_start=0.0)
-    for i in range(0, 4):
-        mdiff.on_to_coordinates(SpeedRPM(40), 0, 600)
-        mdiff.turn_to_angle(SpeedRPM(40), 90)
+    mdiff.on_to_coordinates(SpeedRPM(40), 0, side_length)
+    mdiff.turn_to_angle(SpeedRPM(40), 90),
+    mdiff.on_to_coordinates(SpeedRPM(40), 0, side_length)
+    mdiff.turn_to_angle(SpeedRPM(40), 90)
+    mdiff.on_to_coordinates(SpeedRPM(40), 0, side_length)
+    mdiff.turn_to_angle(SpeedRPM(40), 90)
     mdiff.on_to_coordinates(SpeedRPM(40), 0, 0)
     mdiff.turn_to_angle(SpeedRPM(40), 90)
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
