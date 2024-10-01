511166f64225e6b4dfb0a4513f32c9b773d9a4c6
ALEX RIVAS MACHIN <123485397+AlexRivasMachin@users.noreply.github.com>
2024-10-01 18:25:27 +0200

    movedifferential_cw works, and WIP with ccww

--- circle.py
+++ circle.py
@@ -5,17 +5,12 @@ from ev3dev2.wheel import EV3EducationSetTire # wheel gurpilaren zirkunferentzia
 
 from time import sleep
 
-wheel_circumference = 176  # mm
 side_length = 800  # mm (80 cm)
 
 def main():
     mdiff = MoveDifferential(OUTPUT_A, OUTPUT_C, EV3EducationSetTire, 118)
     mdiff.odometry_start(theta_degrees_start=0.0, x_pos_start=0.0, y_pos_start=0.0, sleep_time=0.005)
-    mdiff.on_to_coordinates(SpeedRPM(40), side_length, 0)
-    mdiff.on_to_coordinates(SpeedRPM(40), side_length, side_length)
-    mdiff.on_to_coordinates(SpeedRPM(40), 0, side_length)
-    mdiff.on_to_coordinates(SpeedRPM(40), 0, 0)
-    mdiff.turn_to_angle(SpeedRPM(40), 0)
+    mdiff.on_arc_right(SpeedRPM(80), 400, 2*1256)
     mdiff.odometry_stop()
 
 if __name__ == "__main__":
--- square_moveddiferrential_cw.py
+++ square_moveddiferrential_cw.py
@@ -10,15 +10,13 @@ side_length = 800  # mm (80 cm)
 
 def main():
     mdiff = MoveDifferential(OUTPUT_A, OUTPUT_C, EV3EducationSetTire, 118)
-    mdiff.odometry_start(theta_degrees_start=0.0)
-    mdiff.on_to_coordinates(SpeedRPM(40), 0, side_length)
-    mdiff.turn_to_angle(SpeedRPM(40), 90),
-    mdiff.on_to_coordinates(SpeedRPM(40), 0, side_length)
-    mdiff.turn_to_angle(SpeedRPM(40), 90)
-    mdiff.on_to_coordinates(SpeedRPM(40), 0, side_length)
-    mdiff.turn_to_angle(SpeedRPM(40), 90)
+    mdiff.odometry_start(theta_degrees_start=0.0, x_pos_start=0.0, y_pos_start=0.0, sleep_time=0.005)
+    mdiff.on_to_coordinates(SpeedRPM(40), side_length, 0)
+    mdiff.on_to_coordinates(SpeedRPM(40), side_length, -side_length)
+    mdiff.on_to_coordinates(SpeedRPM(40), 0, -side_length)
     mdiff.on_to_coordinates(SpeedRPM(40), 0, 0)
-    mdiff.turn_to_angle(SpeedRPM(40), 90)
+    mdiff.turn_to_angle(SpeedRPM(40), 0)
+    mdiff.odometry_stop()
 
 if __name__ == "__main__":
     main()
