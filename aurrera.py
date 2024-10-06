4a69c8e0b9d69e902832af6abb6b4d94eb605c5e
ALEX RIVAS MACHIN <123485397+AlexRivasMachin@users.noreply.github.com>
2024-10-06 12:58:18 +0200

    Update aurrera.py

--- aurrera.py
+++ aurrera.py
@@ -6,10 +6,10 @@ from time import sleep
 
 def main():
     motor = MoveTank(OUTPUT_A, OUTPUT_C)
-    while True:
-        motor.on_for_rotations(left_speed=50, right_speed = 50, rotations = 4.5)
-        motor.on_for_rotations(left_speed=100, right_speed = 0, rotations = 1.15)
-
+    #while True:
+    #    motor.on_for_rotations(left_speed=50, right_speed = 50, rotations = 4.5)
+    #    motor.on_for_rotations(left_speed=100, right_speed = 0, rotations = 1.15)
+    motor.on_for_seconds(left_speed=100, right_speed=100, seconds=10)
 
 if __name__=="__main__":
     main()
