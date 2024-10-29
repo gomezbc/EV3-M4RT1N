02f9e1aacfb567f16c97763a58e4904d43239833
ALEX RIVAS MACHIN <123485397+AlexRivasMachin@users.noreply.github.com>
2024-10-29 18:23:54 +0100

    Update rayo_m4rt1n.py

--- rayo_m4rt1n.py
+++ rayo_m4rt1n.py
@@ -7,54 +7,63 @@ from ev3dev2.sensor import Sensor, INPUT_3
 from ev3dev2.motor import MoveTank, OUTPUT_C, OUTPUT_A
 
 from time import sleep
-SPEED = 55
+SPEED = 40
 
 sensorWeight = {
-    0: 7,
-    1: 5,
-    2: 3,
-    3: 1,
-    4: 1,
-    5: 3,
-    6: 5,
-    7: 7
+    0: 30,
+    1: 40,
+    2: 60,
+    3: 80,
+    4: 80,
+    5: 60,
+    6: 40,
+    7: 30
 }
 
 motorSpeed = {
-    0: 2,
-    1: 4,
-    2: 4,
-    3: 4,
-    4: 5,
-    5: 6,
-    6: 8,
-    7: 10,
-    8: 12,
-    9: 16,
-    10: 20,
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 1,
+    4: 2,
+    5: 3,
+    6: 3,
+    7: 4,
+    8: 5,
+    9: 10,
+    10: 11,
 }
 
-LOGS_FILE = 'logs.txt'
+indexPenalty ={
+    0: 1,
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1,
+    5: 1,
+    6: 1,
+    7: 3,
+    8: 4,
+    9: 7,
+    10: 10,
+}
 
-def writeInfo(left, right):
-    with open(LOGS_FILE, 'a') as f:
-        f.write("L: "+str(left) + " R: " + str(right) + " ABS: " + str(abs(left-right))+"\n")
+
+## TENEMOS QUE MIRAR SIEMPRE EL CASO => 012 -- 765
 
 def hasToSteer(left, right):
-    writeInfo(left, right)
-    return abs(left - right) > 200
+    return left >= 7000 or right >= 7000
 
 def getNewSpeed(left, right):
-    # mirar como ajustar mejor la velocidad de la rueda que no tiene que corregir
-    speedLeft = (left // 160)
-    speedRight = (right // 160)
-    if(speedRight > speedLeft):
-         speedLeft = speedLeft//2
+    leftIndex = min(left // 1600, 10) 
+    rightIndex = min(right // 1600, 10)
+    leftSpeed = SPEED - motorSpeed[leftIndex]
+    rightSpeed = SPEED - motorSpeed[rightIndex]
+    if(leftSpeed > rightSpeed):
+        rightSpeed = rightSpeed - indexPenalty[rightIndex]
     else:
-        speedRight = speedRight//2
-    with open(LOGS_FILE, 'a') as f:
-        f.write("speedLeft: "+str(speedLeft) + " speedRight: " + str(speedRight) + "\n")
-    return SPEED - motorSpeed[speedLeft], SPEED - motorSpeed[speedRight]
+        leftSpeed = leftSpeed - indexPenalty[leftIndex]
+    return leftSpeed, rightSpeed
 
 
 def getSensorWeigths(lsa):
@@ -66,18 +75,27 @@ def getSensorWeigths(lsa):
         right = right + (lsa.value(i) * sensorWeight[i])
     return left, right
 
+def getMidSensorWeigths(lsa):
+    mid = 0
+    for i in range (3,5):
+        mid = mid + (lsa.value(i)*sensorWeight[i]) # MAX 16000
+    return mid
+
 def main():
     tank_drive = MoveTank(OUTPUT_A, OUTPUT_C)
     tank_drive.on(SPEED,SPEED)
     lsa = Sensor(INPUT_3)
     while True:
         left, right = getSensorWeigths(lsa)
-        if hasToSteer(left, right):
+        mid = getMidSensorWeigths(lsa)
+        if hasToSteer(left, right) and mid > 6500:
            speedLeft, speedRight = getNewSpeed(left, right)
            tank_drive.on(speedLeft, speedRight)
+        elif left <= 6000 and right <= 6000:
+            tank_drive.on(SPEED+10,SPEED+10)
         else:
             tank_drive.on(SPEED,SPEED)
-        sleep(0.05)
+        sleep(0.01)
 
 if __name__ == "__main__":
     main()
